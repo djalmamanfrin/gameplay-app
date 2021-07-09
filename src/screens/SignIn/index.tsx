287d5669e0761ef252b0aadee7ada8482201df69
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-07-09 15:47:53 -0300

    função de navegação da tela de Sign para Home

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,12 +4,18 @@ import {
     Text,
     Image
 } from "react-native";
+import {useNavigation} from "@react-navigation/native";
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import  IllustrationImg from '../../assets/illustration.png';
 import { styles } from "./styles";
 
 export function SignIn() {
+    const navegation = useNavigation();
+    function handleSignIn() {
+        navegation.navigate('Home');
+    }
+
     return (
         <View style={styles.container}>
             <Image
@@ -27,7 +33,7 @@ export function SignIn() {
 
                 <ButtonIcon
                     title="Entrar com o Discord"
-                    activeOpacity={0.8}
+                    onPress={handleSignIn}
                 />
             </View>
         </View>
