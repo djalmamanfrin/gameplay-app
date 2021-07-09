30b4a34c3ee6868ed5703b1d661ae9ee2b218885
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-07-09 15:46:58 -0300

    importação das rotas no App.tsx

--- App.tsx
+++ App.tsx
@@ -6,8 +6,7 @@ import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter"
 import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani"
 import AppLoading from "expo-app-loading"
 
-import { SignIn } from "./src/screens/SignIn";
-import { Background } from "./src/components/Background";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const  [fontsLoaded] = useFonts({
@@ -22,13 +21,13 @@ export default function App() {
   }
 
   return (
-      <Background>
+      <>
           <StatusBar
               barStyle="light-content"
               backgroundColor="transparent"
               translucent
           />
-          <SignIn/>
-      </Background>
+          <Routes/>
+      </>
   )
 }
