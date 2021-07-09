76d6ee6569507e3d657bddf494d9b31e80f753a7
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-07-09 03:56:51 -0300

    Aplicação das fontes, remoção do StatusBar e melhoria no texto

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import {
     View,
     Text,
-    Image,
-    StatusBar
+    Image
 } from "react-native";
 
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -13,11 +12,6 @@ import { styles } from "./styles";
 export function SignIn() {
     return (
         <View style={styles.container}>
-            <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
             <Image
                 source={IllustrationImg}
                 style={styles.image}
@@ -25,7 +19,7 @@ export function SignIn() {
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {'\n'} suas jogatinas {'\n'} facilmente
+                    Conecte-se {'\n'} e Organize suas {'\n'} jogatinas
                 </Text>
                 <Text style={styles.subtitle}>
                     Crie grupos para jogar seus games {'\n'} favoritos com seus amigos
