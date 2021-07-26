8c2ca4b91f5abab91e1f1a220685d57bafc872a3
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-07-26 11:07:14 -0300

    Logbox para ignorar warnings

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {StatusBar} from "react-native";
+import { StatusBar, LogBox } from "react-native";
 
 import { useFonts } from "expo-font"
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter"
@@ -9,6 +9,8 @@ import AppLoading from "expo-app-loading"
 import { Routes } from "./src/routes";
 import { AuthProvider } from "./src/hooks/auth";
 import { Background } from "./src/components/Background";
+
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine']);
 
 export default function App() {
   const  [fontsLoaded] = useFonts({
