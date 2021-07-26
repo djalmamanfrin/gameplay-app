6021420e900e131456675ddd0a52a191113d0f09
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-07-26 11:02:20 -0300

    variáveis de ambiente

---
+++ src/configs/discord.ts
@@ -0,0 +1,7 @@
+const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-f0bbd4ea-b672-40b8-b6e0-2d699259263a';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '868467921352028190';
+const CDN_IMGAE = 'https://cdn.discordapp.com';
+
+export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMGAE }
---
+++ src/configs/index.ts
@@ -0,0 +1,3 @@
+ import * as DiscordEnv from './discord'
+
+ export { DiscordEnv };
