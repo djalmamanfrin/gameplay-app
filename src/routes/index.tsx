8656f8035c12ce024719d062cafb534768d1d9f0
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-07-26 11:34:58 -0300

    Separando rotas publicas de privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Background } from "../components/Background";
+import { SignIn } from "../screens/SignIn";
 
 import { AuthRoutes } from "./auth.routes";
+import { useAuth } from "../hooks/auth";
 
 export function Routes() {
+    const { user } = useAuth();
     return (
         <Background>
             <NavigationContainer>
-                <AuthRoutes/>
+                { user.id ? <AuthRoutes/> : <SignIn/> }
             </NavigationContainer>
         </Background>
     )
