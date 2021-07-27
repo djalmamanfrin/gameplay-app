f0a1c3d3f0e9a4b0b65f2a3c928efcef89629a34
Djalma Manfrin <djalma.manfrin@gmail.com>
2021-07-27 06:44:21 -0300

    bug ao cancelar autorização acesso ao discord

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -21,7 +21,8 @@ type ContextData = {
 
 type AuthorizationDiscordResponse = AuthSession.AuthSessionResult & {
     params: {
-        access_token: string;
+        access_token?: string;
+        error?: string;
     }
 }
 
@@ -40,8 +41,9 @@ function AuthProvider({ children } : AuthProviderProps) {
             setLoading(true);
             const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${DiscordEnv.CLIENT_ID}&redirect_uri=${DiscordEnv.REDIRECT_URI}&response_type=${DiscordEnv.RESPONSE_TYPE}&scope=${DiscordEnv.SCOPE}`;
             const {type, params} = await AuthSession.startAsync( {authUrl} ) as AuthorizationDiscordResponse;
+            console.log(type, params)
 
-            if (type === "success") {
+            if (type === "success" && !params.error) {
                 api.defaults.headers.authorization = `Bearer ${ params.access_token }`;
                 const userInfo = await api.get('/users/@me');
                 const firstName = userInfo.data.username.split(' ')[0];
